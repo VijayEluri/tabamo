06c207018a43d0a637dbee256455b4f97cf67b8d
tabamo <tabamo@tabamo-pc-dev.korea.ncsoft.corp>
2013-02-06 17:50:35 +0900

    initialize

---
+++ chapter04.Google/src/chapter04/google/Google.java
@@ -0,0 +1,26 @@
+package chapter04.google;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import chapter04.searchengine.SearchEngine;
+
+public class Google implements SearchEngine{
+	public List<String> search(String query) {
+		List<String> result = new ArrayList<String>();
+		
+		result.add("Google text reuslt1");
+		result.add("Google text result2");
+		
+		return result;
+	}
+	
+	public List<String> searchImage(String query) {
+		List<String> result = new ArrayList<String>();
+		
+		result.add("Google text reuslt1");
+		result.add("Google text result2");
+		
+		return result;
+	}
+}
