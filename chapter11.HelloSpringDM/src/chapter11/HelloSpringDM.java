93c8348ded0f4689b516e5e2f5ba79feb7559046
JunsungPark <tabamo@gmail.com>
2013-02-07 01:54:15 +0900

    initialize

---
+++ chapter11.HelloSpringDM/src/chapter11/HelloSpringDM.java
@@ -0,0 +1,11 @@
+package chapter11;
+
+public class HelloSpringDM {
+	public void start() {
+		System.out.println("Hello spring dm");
+	}
+	
+	public void stop() {
+		System.out.println("Goodbye spring dm");
+	}
+}
